58d1c485b3dcbee255b6c359d859fd65ded75c4d
Yi-wen LIN <bongiovi015@gmail.com>
2013-02-13 16:00:18 -0800

    Polygon Light

--- polygon/js/Index.js
+++ polygon/js/Index.js
@@ -1,7 +1,7 @@
 (function() {
 	var W, H, NUM;
 	W = window.innerWidth;
-	H = window.innerHeight;
+	H = window.innerHeight*2;
 	NUM = 20;
 
 	Index = function(map, gl) {
@@ -21,6 +21,11 @@
 	var p = Index.prototype;
 
 	p.init = function() {
+        this.stats = new Stats();
+        this.stats.domElement.style.position = 'absolute';
+        this.stats.domElement.style.top = '0px';
+        document.body.appendChild( this.stats.domElement );
+		
 		var that = this;
         google.maps.event.addListener(this.map, 'click', function(event) {
 			that._placeMarker(event.latLng);
@@ -101,20 +106,24 @@
                 this.model.updateTextCoord(index*4+2, 1, 1);
                 this.model.updateTextCoord(index*4+3, 0, 1); 
 				
-				var v01 = vec3.subtract(p0, p1);
-				var v21 = vec3.subtract(p2, p1);
-				var n0 = vec3.cross(v01, v21);
-				vec3.normalize(n0);
+				// var v01 = vec3.subtract(p0, p1);
+				// var v21 = vec3.subtract(p2, p1);
+				// var n0 = vec3.cross(v01, v21);
+				// vec3.normalize(n0);
 				
-				var v03 = vec3.subtract(p0, p3);
-				var v23 = vec3.subtract(p2, p3);
-				var n1 = vec3.cross(v23, v03);
-				vec3.normalize(n1);
+				n0 = computeSurfaceNormal(p0, p1, p2);
 				
-				this.model.updateAttribute(0, index*4,     [ n0[0], n0[1], n0[2] ]);
-				this.model.updateAttribute(0, index*4+1,   [ n0[0], n0[1], n0[2] ]);
-				this.model.updateAttribute(0, index*4+2,   [ n0[0], n0[1], n0[2] ]);
-				this.model.updateAttribute(0, index*4+3,   [ n1[0], n1[1], n1[2] ]);
+				// var v03 = vec3.subtract(p0, p3);
+				// var v23 = vec3.subtract(p2, p3);
+				// var n1 = vec3.cross(v23, v03);
+				// vec3.normalize(n1);
+				
+				n1 = computeSurfaceNormal(p0, p2, p3);
+				
+				this.model.updateAttribute(0, index*4,     n0);
+				this.model.updateAttribute(0, index*4+1,   n0);
+				this.model.updateAttribute(0, index*4+2,   n0);
+				this.model.updateAttribute(0, index*4+3,   n0);
             }
         }
 		
@@ -128,25 +137,46 @@
 	
 	
 	p.render = function() {
+		this.stats.update();
 		if(this.p1 == undefined) return;
         this.gl.viewport(0, 0, this.gl.viewportWidth, this.gl.viewportHeight);
         this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
         this.gl.enable(this.gl.BLEND);
-        this.gl.enable(this.gl.DEPTH_TEST);
+        this.gl.disable(this.gl.DEPTH_TEST);
         this.gl.blendFunc(this.gl.SRC_ALPHA, this.gl.ONE_MINUS_SRC_ALPHA);
+		renderImage(this.gl, texture);
+		this.gl.enable(this.gl.DEPTH_TEST);
 		
         var lightingDirection = [this.lightX, this.lightY, this.lightZ];
         var adjustedLD = vec3.create();
         vec3.normalize(lightingDirection, adjustedLD);
         vec3.scale(adjustedLD, -1);
 
+		var matrix = this.camera.update();
         var normalMatrix = mat3.create();
-        mat4.toInverseMat3(this.camera.update(), normalMatrix);
+        mat4.toInverseMat3(matrix, normalMatrix);
         mat3.transpose(normalMatrix);
         this.shader.setParameter("uNMatrix", "uniformMatrix3fv", normalMatrix);
 		this.shader.setParameter("uLightingDirection", "uniform3fv", adjustedLD);
 
-        this.model.render(this.shader, this.camera.update(), this.projection.matrix);
+        this.model.render(this.shader, matrix, this.projection.matrix);
+	}
+	
+	
+	var computeSurfaceNormal = function(p1, p2, p3) {
+		var tmp = [];
+		
+		var u = vec3.subtract(p2, p1);
+		var v = vec3.subtract(p3, p1);
+		tmp[0] = u[1] * v[2] - u[2] * v[1];
+		tmp[1] = u[2] * v[0] - u[0] * v[2];
+		tmp[2] = u[0] * v[1] - u[1] * v[0];
+		
+		var v = vec3.create(tmp);
+		vec3.normalize(v);
+		tmp = [v[0], v[1], v[2]];
+		
+		return tmp;
 	}
 
 })();
